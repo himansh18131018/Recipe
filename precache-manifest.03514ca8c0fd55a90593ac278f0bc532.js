78c1e8a2c8c8054af6faf318f47186fbe54fdd65
unknown <Himansh181310@gmail.com>
2021-07-29 00:24:21 +0530

    Updates

---
+++ precache-manifest.03514ca8c0fd55a90593ac278f0bc532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418d58cd38ab7584b0805e7d8099dafc",
+    "url": "/Recipe/index.html"
+  },
+  {
+    "revision": "59cf4a41fc26cac617bc",
+    "url": "/Recipe/static/css/main.57b1883b.chunk.css"
+  },
+  {
+    "revision": "7753739649a2938da221",
+    "url": "/Recipe/static/js/2.3c795e18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe/static/js/2.3c795e18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59cf4a41fc26cac617bc",
+    "url": "/Recipe/static/js/main.d628cb29.chunk.js"
+  },
+  {
+    "revision": "efc72d8c1e1b0dd60db5",
+    "url": "/Recipe/static/js/runtime-main.737d7316.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Recipe/static/media/logo.5d5d9eef.svg"
+  }
+]);
